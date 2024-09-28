a2cad1619434da740d94410c1493d7d5b019af90
warmachine028 <75939390+warmachine028@users.noreply.github.com>
2024-09-28 12:31:44 +0530

    feat: scafolding->themes

--- client/src/lib/utils.js
+++ client/src/lib/utils.js
@@ -1,6 +1,16 @@
-import { clsx } from "clsx";
-import { twMerge } from "tailwind-merge"
+import { clsx } from 'clsx'
+import { twMerge } from 'tailwind-merge'
 
-export function cn(...inputs) {
-  return twMerge(clsx(inputs));
+export const cn = (...inputs) => twMerge(clsx(inputs))
+
+export const getWords = (text, specialLastWord, className) => {
+	return text.split(' ').map((word, idx) => {
+		return {
+			text: word,
+			className:
+				specialLastWord && idx === text.split(' ').length - 1
+					? 'text-blue-500 dark:text-blue-500'
+					: '' + className
+		}
+	})
 }
