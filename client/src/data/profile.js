a943dd2fed3e0eff76090c67eea8d6b7ec8fea4c
Srija Adhya <113666002+SrijaAdhya12@users.noreply.github.com>
2024-09-28 14:32:29 +0530

    feat: added profile data

---
+++ client/src/data/profile.js
@@ -0,0 +1,19 @@
+export const sleepData = [
+	{ day: 'Mon', hours: 7 },
+	{ day: 'Tue', hours: 6.5 },
+	{ day: 'Wed', hours: 8 },
+	{ day: 'Thu', hours: 7.5 },
+	{ day: 'Fri', hours: 6 },
+	{ day: 'Sat', hours: 9 },
+	{ day: 'Sun', hours: 8.5 }
+]
+
+export const exerciseData = [
+	{ day: 'Mon', minutes: 30 },
+	{ day: 'Tue', minutes: 45 },
+	{ day: 'Wed', minutes: 0 },
+	{ day: 'Thu', minutes: 60 },
+	{ day: 'Fri', minutes: 30 },
+	{ day: 'Sat', minutes: 90 },
+	{ day: 'Sun', minutes: 45 }
+]
