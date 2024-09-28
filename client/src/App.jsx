ca182d7fec54d0fbaafdf48a16ec0d646768437a
warmachine028 <75939390+warmachine028@users.noreply.github.com>
2024-09-28 13:21:58 +0530

    feat: added theme context

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,27 @@
-import './App.css'
-import { Button } from './components/ui/button'
+import { BrowserRouter } from 'react-router-dom'
+import { Auth0Provider } from '@auth0/auth0-react'
+import { Toaster } from '@/components/ui/toaster'
+import { ThemeProvider } from '@/providers'
 
-function App() {
+const PrismaAI = () => {
 	return (
-		<>
-			<Button>Hello</Button>
-		</>
+		<ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
+			<Toaster />
+		</ThemeProvider>
+	)
+}
+
+const App = () => {
+	return (
+		<Auth0Provider
+			domain={import.meta.env.VITE_AUTH0_DOMAIN}
+			clientId={import.meta.env.VITE_AUTH0_CLIENT_ID}
+			authorizationParams={{ redirect_uri: window.location.origin }}
+		>
+			<BrowserRouter>
+				<PrismaAI />
+			</BrowserRouter>
+		</Auth0Provider>
 	)
 }
 
