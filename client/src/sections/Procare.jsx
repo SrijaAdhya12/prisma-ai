4a49e013612517bdfbed955f514209766c6d81cf
Srija Adhya <113666002+SrijaAdhya12@users.noreply.github.com>
2024-09-29 01:19:46 +0530

    feat: added procare button

--- client/src/sections/Procare.jsx
+++ client/src/sections/Procare.jsx
@@ -1,6 +1,13 @@
+import { Button } from "@/components/ui/button"
+import { ClipboardPlusIcon } from "lucide-react"
+import { Link } from "react-router-dom"
+
 const Procare = () => {
 	return (
-		<section id="procare" className="mx-4 sm:container sm:mx-auto rounded-2xl bg-opacity-30 py-12 shadow-xl ring-1 ring-gray-900/10  md:py-24 lg:py-32">
+		<section
+			id="procare"
+			className="mx-4 rounded-2xl bg-opacity-30 py-12 shadow-xl ring-1 ring-gray-900/10 sm:container sm:mx-auto md:py-24 lg:py-32"
+		>
 			<div className="container mx-auto px-4 md:px-6">
 				<div className="grid items-center gap-6 lg:grid-cols-2 lg:gap-12">
 					<div className="absolute left-0 right-0 z-10 space-y-4 px-10 sm:static">
@@ -12,6 +19,12 @@ const Procare = () => {
 							provide more personalized, interactive sessions. Both options are confidential, ensuring
 							privacy and security for every user.
 						</p>
+						<div className="flex py-6 justify-center sm:justify-start">
+							<Button>
+								<Link to="/pro-care">Connect Instantly</Link>
+								<ClipboardPlusIcon className="ml-2 h-4 w-4" />
+						</Button>
+						</div>
 					</div>
 					<div className="relative flex justify-center">
 						<div className="aspect-square w-full max-w-60 rounded-full bg-gradient-to-br from-purple-500 to-pink-500 opacity-60 blur-2xl sm:max-w-[400px]" />
