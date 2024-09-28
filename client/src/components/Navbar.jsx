0335cc87e799ddf3cef5d990f828545646556a57
warmachine028 <75939390+warmachine028@users.noreply.github.com>
2024-09-28 14:33:33 +0530

    feat: Loader.jsx

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react'
 import { Menu, X, Brain } from 'lucide-react'
 import { Link, NavLink, useLocation } from 'react-router-dom'
 import { cn } from '@/lib'
-import { AvatarButton, Loader } from '.'
+import { AvatarButton,  } from '.'
 import { useAuth0 } from '@auth0/auth0-react'
 import { motion, AnimatePresence } from 'framer-motion'
 
@@ -11,7 +11,17 @@ const Navbar = ({ initialBackground, navItems = [] }) => {
 	const [background, setBackground] = useState(initialBackground)
 	const [isOpen, setIsOpen] = useState(false)
 
-	
+	const smoothScrollTo = (target) => {
+		document.querySelector(target)?.scrollIntoView({ behavior: 'smooth' })
+		setIsOpen(false)
+	}
+
+	useEffect(() => {
+		const toggleBackground = () =>
+			setBackground(window.scrollY > 130 ? 'bg-transparent border-b' : initialBackground)
+		window.addEventListener('scroll', toggleBackground)
+		return () => window.removeEventListener('scroll', toggleBackground)
+	}, [initialBackground])
 
 	return (
 		<>
@@ -29,9 +39,30 @@ const Navbar = ({ initialBackground, navItems = [] }) => {
 								Prisma AI
 							</Link>
 						</div>
-	
+						<ul className="hidden items-center space-x-7 font-medium tracking-wide lg:flex">
+							{navItems.map((item) =>
+								!isAuthenticated && item.private ? null : (
+									<li
+										key={item.label}
+										className="text-foreground/80 hover:text-foreground flex items-center gap-2 rounded-full p-3 transition-colors"
+									>
+										{item.target && (
+											<button onClick={() => smoothScrollTo(item.target)}>{item.label}</button>
+										)}
+										{item.to && (
+											<NavLink to={item.to} className="flex items-center gap-2">
+												{item.label}
+											</NavLink>
+										)}
+										{item.icon && <item.icon className="size-4" />}
+									</li>
+								)
+							)}
+						</ul>
 					</div>
-	
+					<div className="hidden items-center space-x-2 lg:flex">
+						<AvatarButton />
+					</div>
 				</div>
 			</nav>
 			<AnimatePresence>
