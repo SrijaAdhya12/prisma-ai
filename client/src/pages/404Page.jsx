70733e75deb674465f3f5a40e3fe20a557f5ed47
warmachine028 <75939390+warmachine028@users.noreply.github.com>
2024-09-28 15:34:20 +0530

    feat: 404 page responsiveness

--- client/src/pages/404Page.jsx
+++ client/src/pages/404Page.jsx
@@ -4,15 +4,15 @@ import { Link } from 'react-router-dom'
 
 const Error404Page = () => {
 	return (
-		<main className="flex h-full items-center" id="404-page">
-			<div className="flex min-h-screen flex-col">
+		<main className="flex h-full items-center justify-center" id="404-page">
+			<div className="flex min-h-screen flex-col items-center justify-center gap-2">
 				<AlertCircle className="text-destructive h-16 w-16" />
-				<h1 className="mt-4 text-2xl text-xl sm:text-4xl">Page Not Found</h1>
-				<p className="mt-2 text-center text-sm font-bold sm:text-lg">
+				<h1 className="mt-4 text-2xl font-bold sm:text-4xl">Page Not Found</h1>
+				<p className="mt-2 text-center text-sm sm:text-lg">
 					Oops! The page you are looking for does not exist.
 				</p>
 				<img src={Error404} alt="404 Error" className="mt-8 w-2/3 max-w-md" />
-				<Link to="/" className="bg-primary/85 hover:bg-primary mb-9 mt-4 rounded px-4 py-2 transition-colors">
+				<Link to="/" className="bg-primary/85 hover:bg-primary mt-4 rounded px-4 py-2 transition-colors">
 					Go Back Home
 				</Link>
 			</div>
