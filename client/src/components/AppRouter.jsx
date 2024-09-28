f07e230f3949b3cfe50d00056bca87b690e0d1fd
Srija Adhya <113666002+SrijaAdhya12@users.noreply.github.com>
2024-09-28 15:38:43 +0530

    fix: app router

--- client/src/components/AppRouter.jsx
+++ client/src/components/AppRouter.jsx
@@ -1,12 +1,42 @@
-import { Therafy } from '@/pages/therafy'
-import { Route, Routes, useLocation } from 'react-router-dom'
+import { Home, ComingSoon } from '@/pages'
+import { PrivateRoute } from '@/routes'
+import { Navigate, Route, Routes, useLocation } from 'react-router-dom'
 
 const AppRouter = () => {
 	const location = useLocation()
 	return (
 		<Routes location={location}>
-			<Route path="/" element={<div />} />
-			<Route path="/therafy" element={<Therafy />} />
+			<Route path="/" element={<Home />} />
+			<Route path="/chat" element={<PrivateRoute component={<ComingSoon />} />} />
+			<Route path="/dashboard" element={<PrivateRoute component={<ComingSoon />} />}>
+				<Route path="" element={<Navigate to="profile" />} />
+				<Route path="profile" element={<ComingSoon />} />
+				<Route path="settings" element={<ComingSoon />} />
+				<Route path="goals" element={<ComingSoon />} />
+				<Route path="*" element={<ComingSoon />} />
+			</Route>
+			<Route path="/emo-sense" element={<PrivateRoute component={<ComingSoon />} />}>
+				<Route path="" element={<Navigate to="mood-sense" />} />
+				<Route path="mood-sense" element={<ComingSoon />} />
+				<Route path="video-sense" element={<ComingSoon />} />
+				<Route path="support-bot" element={<ComingSoon />} />
+				<Route path="*" element={<ComingSoon />} />
+			</Route>
+			<Route path="/therafy" element={<PrivateRoute component={<ComingSoon />} />}>
+				<Route path="" element={<Navigate to="mood-music" />} />
+				<Route path="mood-music" element={<ComingSoon />} />
+				<Route path="content-theraphy" element={<ComingSoon />} />
+				<Route path="physical-theraphy" element={<ComingSoon />} />
+				<Route path="*" element={<ComingSoon />} />
+			</Route>
+			<Route path="/pro-care" element={<PrivateRoute component={<ComingSoon />} />}>
+				<Route path="" element={<Navigate to="chat" />} />
+				<Route path="chat" element={<ComingSoon />} />
+				<Route path="video" element={<ComingSoon />} />
+				<Route path="*" element={<ComingSoon />} />
+			</Route>
+			<Route path="/support-nest" element={<ComingSoon />} />
+			<Route path="*" element={<ComingSoon />} />
 		</Routes>
 	)
 }
